f13ab318e490cabf186d054e6b90b50b692dce7f
CCapslock <53517856+CCapslock@users.noreply.github.com>
2021-02-03 21:01:52 +0300

    закинул проект

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+	private Rigidbody _rb;
+	private void Start()
+	{
+		_rb = GetComponent<Rigidbody>();
+	}
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Debug.Log("Collided with " + other.gameObject.name);
+			this.gameObject.tag = "Player";
+			_rb.isKinematic = false;
+		}
+	}
+}
---
+++ Assets/Scripts/RotatingController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RotatingController : MonoBehaviour
+{
+    private Transform _sligshotTransform;
+    private InputController _inputController;
+
+    private Vector3 _startPosition;
+    private Vector3 _rotatingVector;
+    private bool _delayCounted;
+
+    private void Start()
+    {
+        _sligshotTransform = GetComponent<Transform>();
+        _inputController = FindObjectOfType<InputController>();
+    }
+    private void FixedUpdate()
+    {
+        if (_inputController.DragingStarted)
+        {
+            if (!_delayCounted)
+            {
+                _startPosition = _inputController.TouchPosition;
+                _delayCounted = true;
+            }
+
+            Vector2 tempVector = new Vector2(_inputController.TouchPosition.x - _startPosition.x, _inputController.TouchPosition.y - _startPosition.y);
+            RotateAtSide(tempVector);
+        }
+        else
+        {
+            _delayCounted = false;
+        }
+    }
+    private void RotateAtSide(Vector2 vector)
+    {
+        _rotatingVector = Vector3.zero;
+        _rotatingVector.y = vector.x;
+        _sligshotTransform.Rotate(_rotatingVector);
+        _rotatingVector = Vector3.zero;
+        _rotatingVector.x =vector.y;
+        _sligshotTransform.Rotate(_rotatingVector);
+        if(_sligshotTransform.rotation.eulerAngles.z != 0)
+        {
+            _rotatingVector = Vector3.zero;
+            _rotatingVector.z = _sligshotTransform.rotation.eulerAngles.z * -1;
+            _sligshotTransform.Rotate(_rotatingVector);
+        }
+    }
+}
