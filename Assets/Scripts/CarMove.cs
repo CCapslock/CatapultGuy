6eeeb92366935efb92085ffe9af901a41b47f8c6
egorshko <66785233+egorshko@users.noreply.github.com>
2021-02-26 20:16:36 +0300

    New Models

---
+++ Assets/Scripts/CarMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    private float _speed = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/VehicleMoveing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleMoveing : MonoBehaviour
+{
+    
+    
+
+    private float _speed = 30f;
+    
+
+    
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        
+    }
+
+   
+}
